b5db6869567beadfaacc9c67a6dbb884b1b5a5ba
Akobi <akobirdeveloper@icloud.com>
2025-06-27 12:12:37 +0500

    i fineshed my part and it's time you diyorbek

--- front/src/App.js
+++ front/src/App.js
@@ -11,6 +11,8 @@ import AdminNews from "./admin/admin/AdminNews";
 import DataForm from "./pages/dataForm/DataForm";
 import History from "./pages/history/History";
 import Appeal from "./pages/appeal/Appeal";
+import CastingUser from "./admin/admin/CastingUser";
+import CastingUserDetail from "./admin/admin/CastingUserDetail";
 
 
 
@@ -51,8 +53,8 @@ function App() {
         {/*  app admin */}
         <Route path={"/admin"} element={<AdminHome/>}/>
         <Route path={"/admin/news"} element={<AdminNews/>}/>
-        <Route path={"/admin/casting-users"} element={<AdminHome/>}/>
-        <Route path={"/admin/casting-users/:userId"} element={<AdminHome/>}/>
+        <Route path={"/admin/casting-users"} element={<CastingUser/>}/>
+        <Route path={"/admin/casting-users/:castingUserId"} element={<CastingUserDetail/>}/>
         <Route path={"/*"} element={<PageNotFound/>}/>
 
 
